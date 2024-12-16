679dab48e14e4ae32d04a23de6107020576522f1
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2024-12-16 21:12:37 +0500

    weather appning js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API = '666f09cb61f24b13aac45021241312';
-const city = 'Tashkent'
 const container = document.querySelector('.container')
+const footer = document.querySelector('.footer')
 const cities = [
   "Tashkent",
   "New York City",
@@ -24,27 +24,31 @@ const cities = [
   "Cairo",
   "Lagos"
 ];
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-cities.forEach(city => {
+cities.forEach((city) => {
   document.getElementById('city-select').innerHTML += `
     <option value="${city}">${city}</option>
   `
 })
 
 
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-function getWeather(city) {
-  axios.get(`https://api.weatherapi.com/v1/current.json?q=${city}&key=${API}`).then((response) => {
-    const location = response.data.location
-    const current = response.data.current
+function getWeather(city = 'Tashkent') {
+  axios.get(`https://api.weatherapi.com/v1/forecast.json?q=${city}&key=${API}&days=7`).then((response) => {
+    addOption(response.data.forecast.forecastday)
+    displayCurrentWeather(response.data.current, response.data.location, response)
+  })
+}
+getWeather()
+
+function displayCurrentWeather(current, location, response) {
     const time = new Date(current.last_updated)
     const currentDay = time.getDate()
     const currentDayName = daysOfWeek[time.getDay()]
     const currentMonth = shortMonths[time.getMonth()]
     const currentYear = time.getFullYear()
-    
     
     container.innerHTML = `
       <div class="weather-side">
@@ -53,7 +57,7 @@ function getWeather(city) {
         <h2 class="date-dayname">${currentDayName}</h2><span class="date-day">${currentDay} ${currentMonth} ${currentYear}</span><i class="location-icon" data-feather="map-pin"></i><span class="location">${location.name}</span>
       </div>
       <div class="weather-container"><i class="weather-icon" data-feather="sun"></i>
-        <h1 class="weather-temp">${Math.floor(current.temp_c)}°C</h1>
+        <h1 class="weather-temp">${current.temp_c}°C</h1>
         <h3 class="weather-desc">${current.condition.text}</h3>
       </div>
     </div>
@@ -71,14 +75,80 @@ function getWeather(city) {
           </div>
         </div>
       </div>
+    </div>
+    ` 
+    displayHourlyWeather(response.data.forecast.forecastday[0])
+    document.querySelectorAll('#date').forEach(date => {
+      date.addEventListener('click', (e) => {
+        console.log(e.target.value);
+        displayForecastWeather(response.data.forecast.forecastday[e.target.value], response.data.location)
+        displayHourlyWeather(response.data.forecast.forecastday[e.target.value])
+      })
+    })
+}
+function displayHourlyWeather(forecast) {
+  footer.innerHTML = ''
+
+  // console.log(forecast);
+  for (let i = 0; i < forecast.hour.length; i += 3) {
+    const h = forecast.hour[i]
+    footer.innerHTML += `
+    <div><img src="${h.condition.icon}" alt="a\">
+      <p class="degree">${h.temp_c}°C</p><span class="hour">${h.time.slice(-5)}</span>
+    </div>
+    `
+  }
+}
+function displayForecastWeather(forecast, location) {
+  container.innerHTML = ''
+  console.log(forecast);
+  const time = new Date(forecast.date)
+    const currentDay = time.getDate()
+    const currentDayName = daysOfWeek[time.getDay()]
+    const currentMonth = shortMonths[time.getMonth()]
+    const currentYear = time.getFullYear()
+    
+    container.innerHTML = `
+      <div class="weather-side">
+      <div class="weather-gradient"></div>
+      <div class="date-container">
+        <h2 class="date-dayname">${currentDayName}</h2><span class="date-day">${currentDay} ${currentMonth} ${currentYear}</span><i class="location-icon" data-feather="map-pin"></i><span class="location">${location.name}</span>
+      </div>
+      <div class="weather-container"><i class="weather-icon" data-feather="sun"></i>
+        <h1 class="weather-temp">${forecast.day.avgtemp_c}°C</h1>
+        <h3 class="weather-desc">${forecast.day.condition.text}</h3>
+      </div>
+    </div>
+    <div class="info-side">
+      <div class="today-info-container">
+        <div class="today-info">
+          <div class="precipitation"> <span class="title">PRECIPITATION</span><span class="value">${forecast.hour[0].precip_mm} %</span>
+            <div class="clear"></div>
+          </div>
+          <div class="humidity"> <span class="title">HUMIDITY</span><span class="value">${forecast.hour[0].humidity} %</span>
+            <div class="clear"></div>
+          </div>
+          <div class="wind"> <span class="title">WIND</span><span class="value">${forecast.hour[0].wind_kph} km/h</span>
+            <div class="clear"></div>
+          </div>
+        </div>
+      </div>
       
     </div>
     ` 
+}
+function addOption(forecastDay) {
+  forecastDay.forEach((day,index) => {
+    document.querySelector('#date-select').innerHTML += `
+      <option value="${index}" id="date">${day.date.slice(-2)}</option>
+    `
   })
 }
-getWeather(city)
-
 
 document.getElementById('city-select').addEventListener('change', (e) => {
   getWeather(e.target.value)
-}) 
+})
+
+
+
+
